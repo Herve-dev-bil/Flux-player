f5f5f66191492dae036b1c8ffb9e3a048f342dfd
Game_Changer <yonkeupavel@gmail.com>
2026-07-08 18:17:06 +0100

    Merge real app code into root, remove duplicate nested folder

---
+++ android/app/src/main/kotlin/com/gamechanger/flux_player_main/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.gamechanger.flux_player_main
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- flux-player-main/lib/features/organizer/organizer_providers.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'organizer_service.dart';
-
-final organizerServiceProvider =
-    Provider<OrganizerService>((ref) => OrganizerService());
-
-final organizeModeProvider =
-    StateProvider<OrganizeMode>((ref) => OrganizeMode.recentlyAdded);
